3f7a8151b682c564b99e89b7053b27b77c1af030
Chris Kansas <ckansas@smcm.edu>
2017-07-27 17:52:28 -0600

    Started the bare bones of the text adventure.

---
+++ TextAdventure1.py
@@ -0,0 +1,29 @@
+#! python3
+
+#This will be my first foray into the land of text adventure games.
+
+import actions, time, sys
+
+#TODO Provide the setting
+
+setting = "You wake up to find yourself covered in blood. You remember you are on a space station. Your name is Jack Harrington and you are a scientist stationed here for research. You are in the barracks where the other researchers sleep; however, there is no one to be seen."
+
+available_actions = "\n \n Actions: \n -Scream \n -Look Around \n -Try to Remember"
+
+for char in setting:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.07)
+print(available_actions)
+
+action = input("What do you do?: ")
+if action == "Scream":
+    actions.scream()
+elif action == "Look Around":
+    actions.look_around()
+elif action == "Try to Remember":
+    actions.try_to_remember()
+else:
+    print("Please enter one of the provided commands")
+    print(available_actions)
+    
---
+++ actions.py
@@ -0,0 +1,11 @@
+#! python3
+
+#This file will contain the functions that the user can call to perform actions.
+def scream():
+    print("You scream for help! 'HELP!!!!'")
+
+def look_around():
+    print("You look back and forth. You look down at your blook soaked clothing. You realize that the floor is covered in blood as well and the trail leads out of the barracks and into the hallway.")
+
+def try_to_remember():
+    print("You sit in silence while you try to think back to the past few days. You remember that your fellow researchers were excited due to the recent break through on the alien technology. You can also recall that one of them in particular, Helena, was frightened by the possibilities.")
